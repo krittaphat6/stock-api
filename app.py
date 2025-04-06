ba1c41da456ef085815a6ea3bb390d3914d1a4b2
krittaphat6 <99319518+krittaphat6@users.noreply.github.com>
2025-04-07 02:17:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,22 @@ import yfinance as yf
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'Stock API is running'
+
 @app.route('/stockinfo', methods=['GET'])
 def stock_info():
     symbol = request.args.get('symbol', default='AAPL', type=str)
     stock = yf.Ticker(symbol)
-
+    
     fin = stock.financials
     info = stock.info
 
     latest_eps = info.get("trailingEps", "N/A")
     pe_ratio = info.get("trailingPE", "N/A")
-    revenue = fin.loc['Total Revenue'][0] if 'Total Revenue' in fin.index else "N/A"
-    net_income = fin.loc['Net Income'][0] if 'Net Income' in fin.index else "N/A"
+    revenue = fin.loc["Total Revenue"][0] if "Total Revenue" in fin.index else "N/A"
+    net_income = fin.loc["Net Income"][0] if "Net Income" in fin.index else "N/A"
 
     return jsonify({
         "symbol": symbol,
@@ -22,7 +26,7 @@ def stock_info():
         "net_income": f"{net_income:,}",
         "eps": latest_eps,
         "pe_ratio": pe_ratio,
-        "message": f"งบการเงิน {symbol} เรียบร้อย"
+        "message": f"คุณกำลังดูข้อมูลของ {symbol}"
     })
 
 if __name__ == '__main__':
